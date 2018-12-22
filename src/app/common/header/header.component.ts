6486701bb8ae0b0b2af9d5ee6ae948269423b44d
wyd3x <shuhss@gmail.com>
2018-12-22 21:13:15 +0200

    init

---
+++ src/app/common/header/header.component.ts
@@ -0,0 +1,8 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+    selector: "js-header",
+    templateUrl: "./header.component.html",
+    styleUrls: ["./header.component.scss"]
+})
+export class HeaderComponent { }
